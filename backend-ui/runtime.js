a3b60b392b167668da0715c9d5a209b34e9bd84e
dfreire <dario.freire@gmail.com>
2016-04-28 12:04:03 +0100

    update

--- backend-ui/runtime.js
+++ backend-ui/runtime.js
@@ -1,5 +1,10 @@
 (function() {
 
+	var EVENTS = {
+		OPEN_MAIN_TAB: "OPEN_MAIN_TAB",
+		SELECTED_MAIN_TAB: "SELECTED_MAIN_TAB"
+	};
+
 	var Bus = (function() {
 		var callbacks = []; // [{ eventId: ..., callbackRef: ..., callbackFn: ... }]
 		var nextCallbackRef = 0;
@@ -80,35 +85,54 @@
 	function createMainSectionStack(model) {
 
 		function createMainSection(section) {
+			var treeData = _.map(section.items, function(item) {
+				return {
+					id: item.id,
+					title: item.title,
+					parentId: item.parentId
+				};
+			});
+
+			var treeGrid = isc.TreeGrid.create({
+				showHeader: false,
+				showRoot: false,
+				selectionType: "single",
+				fields: [{
+					name: 'title'
+				}],
+				data: isc.Tree.create({
+					modelType: 'parent',
+					idField: 'id',
+					parentIdField: 'parentId',
+					data: treeData
+				}),
+				selectionUpdated: function(record, recordList) {
+					if (_.size(recordList) > 0) {
+						var tabId = [section.id, recordList[0].id].join(".");
+						Bus.fire(EVENTS.OPEN_MAIN_TAB, tabId);
+					}
+				}
+			});
+
+			Bus.on(EVENTS.SELECTED_MAIN_TAB, function(id) {
+				var recordId = id.split(section.id + ".")[1];
+				var toSelect = [];
+				var toDeselect = [];
+				_.each(treeData, function(record) {
+					if (record.id === recordId) {
+						toSelect.push(record);
+					} else {
+						toDeselect.push(record);
+					}
+				});
+				treeGrid.deselectRecords(toDeselect);
+				treeGrid.selectRecords(toSelect);
+			});
+
 			return {
 				title: section.title,
 				expanded: section.expanded,
-				items: [isc.TreeGrid.create({
-					showHeader: false,
-					showRoot: false,
-					selectionUpdated: function(record, recordList) {
-						if (_.size(recordList) > 0) {
-							var id = [section.id, recordList[0].id].join(".");
-							Bus.fire("selectionUpdated", id);
-							// console.log("selectionUpdated", id);
-						}
-					},
-					fields: [{
-						name: 'title'
-					}],
-					data: isc.Tree.create({
-						modelType: 'parent',
-						idField: 'id',
-						parentIdField: 'parentId',
-						data: _.map(section.items, function(item) {
-							return {
-								id: item.id,
-								title: item.title,
-								parentId: item.parentId
-							};
-						})
-					})
-				})]
+				items: [treeGrid]
 			};
 		}
 
@@ -124,14 +148,43 @@
 	}
 
 	function createMainTabSet(model) {
-		return isc.TabSet.create({
+		var tabset = isc.TabSet.create({
 			autoDraw: false,
 			margin: 2,
 			tabBarPosition: "top",
 			width: "100%",
 			height: "100%",
-			tabs: []
+			tabs: [],
+			tabSelected: function(tabNum, tabPane, ID, tab, name) {
+				Bus.fire(EVENTS.SELECTED_MAIN_TAB, iscIdToModelId(ID));
+			}
 		});
+
+		Bus.on(EVENTS.OPEN_MAIN_TAB, function(tabId) {
+			var iscID = modelIdToIscId(tabId);
+			if (_.isObject(tabset.getTab(iscID))) {
+				tabset.selectTab(iscID);
+			} else {
+				var tab = model[tabId];
+				tabset.addTab({
+					ID: iscID,
+					title: tab.title,
+					canClose: true
+				});
+				tabset.selectTab(iscID);
+			}
+		});
+
+		return tabset;
 	}
+
+	function modelIdToIscId(id) {
+		return "sc__" + id.split(".").join("__");
+	}
+
+	function iscIdToModelId(id) {
+		return _.rest(id.split("__")).join(".");
+	}
+
 
 })();
