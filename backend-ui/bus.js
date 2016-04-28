5c9767026bc5ded6a434de63541c7ca3f7da8cbf
dfreire <dario.freire@gmail.com>
2016-04-28 18:25:05 +0100

    update

---
+++ backend-ui/bus.js
@@ -0,0 +1,47 @@
+var Bus = (function() {
+    var callbacks = []; // [{ eventId: ..., callbackRef: ..., callbackFn: ... }]
+    var nextCallbackRef = 0;
+    var handlers = {};
+
+    return {
+        on: function(eventId, callback) {
+            var item = {
+                eventId: eventId,
+                callbackRef: nextCallbackRef++,
+                callbackFn: callback
+            };
+            callbacks.push(item);
+            return item.callbackRef;
+        },
+        off: function(eventId, callbackRef) {
+            callbacks = _.filter(callbacks, function(item) {
+                item.callbackRef !== callbackRef
+            });
+        },
+        once: function(eventId, callback) {
+            var callbackRef = Bus.on(eventId, _.wrap(callback, function(data) {
+                Bus.off(eventId, callbackRef);
+                Bus.callback(data);
+            }));
+        },
+        fire: function(eventId, data) {
+            console.log("fire", eventId, JSON.stringify(data));
+            _.each(callbacks, function(item) {
+                if (item.eventId === eventId) {
+                    item.callbackFn(data);
+                }
+            });
+        },
+        handle: function(actionId, handler) {
+            handlers[actionId] = handler;
+        },
+        execute: function(actionId, data, callback) {
+            console.log("execute", actionId, JSON.stringify(data));
+            if (_.isFunction(handlers[actionId])) {
+                handlers[actionId](data, callback);
+            } else {
+                throw "No handler for " + actionId;
+            }
+        }
+    };
+})();
--- backend-ui/runtime.js
+++ backend-ui/runtime.js
@@ -1,55 +1,13 @@
 (function() {
 
 	var EVENTS = {
-		OPEN_MAIN_TAB: "OPEN_MAIN_TAB",
-		SELECTED_MAIN_TAB: "SELECTED_MAIN_TAB"
+		CLICKED_MAIN_SECTION_ITEM: "CLICKED_MAIN_SECTION_ITEM",
+		SELECTED_MAIN_TAB: "SELECTED_MAIN_TAB",
 	};
 
-	var Bus = (function() {
-		var callbacks = []; // [{ eventId: ..., callbackRef: ..., callbackFn: ... }]
-		var nextCallbackRef = 0;
-		var handlers = {};
-
-		return {
-			on: function(eventId, callback) {
-				var item = {
-					eventId: eventId,
-					callbackRef: nextCallbackRef++,
-					callbackFn: callback
-				};
-				callbacks.push(item);
-				return item.callbackRef;
-			},
-			off: function(eventId, callbackRef) {
-				callbacks = _.filter(callbacks, function(item) {
-					item.callbackRef !== callbackRef
-				});
-			},
-			once: function(eventId, callback) {
-				var callbackRef = Bus.on(eventId, _.wrap(callback, function(eventData) {
-					Bus.off(eventId, callbackRef);
-					Bus.callback(eventData);
-				}));
-			},
-			fire: function(eventId, eventData) {
-				console.log("fire", eventId, JSON.stringify(eventData));
-				_.each(callbacks, function(item) {
-					if (item.eventId === eventId) {
-						item.callbackFn(eventData);
-					}
-				});
-			},
-			handle: function(callId, callback) {
-				handlers[callId] = callback;
-			},
-			call: function(callId, callData) {
-				if (!_.isFunction(handlers[callId])) {
-					throw "No handler for " + callId;
-				}
-				return handlers[callId](callData);
-			}
-		};
-	})();
+	var ACTIONS = {
+		FETCH_GRID_DATA: "FETCH_GRID_DATA",
+	};
 
 	function createMainMenu(model) {
 		return isc.ToolStrip.create({
@@ -98,7 +56,7 @@
 				rowClick: function(record) {
 					if (_.isObject(record)) {
 						var tabId = [section.id, record.id].join(".");
-						Bus.fire(EVENTS.OPEN_MAIN_TAB, tabId);
+						Bus.fire(EVENTS.CLICKED_MAIN_SECTION_ITEM, tabId);
 					}
 				}
 			});
@@ -149,7 +107,7 @@
 			}
 		});
 
-		Bus.on(EVENTS.OPEN_MAIN_TAB, function(tabId) {
+		Bus.on(EVENTS.CLICKED_MAIN_SECTION_ITEM, function(tabId) {
 			var iscID = modelIdToIscId(tabId);
 			if (_.isObject(tabset.getTab(iscID))) {
 				tabset.selectTab(iscID);
@@ -214,8 +172,10 @@
 			width: "100%",
 			height: "100%",
 			alternateRecordStyles: true,
+			canAutoFitFields: false,
 			showFilterEditor: true,
 			filterOnKeypress: true,
+		    canMultiSort: true,
 			dataProperties: {
 				useClientSorting: true,
 				useClientFiltering: true
@@ -225,11 +185,13 @@
 			dataSource: datasource
 		});
 
-		$.getJSON("http://176.58.98.20/v1/get-customers", function(response) {
-			console.log("response", response);
+		function fetchGridData() {
+			Bus.execute(ACTIONS.FETCH_GRID_DATA, { gridId: id, gridModel: gridModel }, function(data) {
+				datasource.setCacheData(data);
+			});
+		}
 
-			datasource.setCacheData(response.data);
-		});
+		fetchGridData();
 
 		return grid;
 	}
@@ -244,7 +206,21 @@
 
 	$.getJSON("/generated/model.json", function(model) {
 		console.log("model", model);
+		createRules(model);
 		render(model);
 	});
 
+	function createRules(model) {
+		Bus.handle(ACTIONS.FETCH_GRID_DATA, function(requestData, callback) {
+			console.log(ACTIONS.FETCH_GRID_DATA, requestData.gridModel.endpoint);
+
+			$.getJSON(requestData.gridModel.endpoint, function(response) {
+				if (response.ok === true) {
+					callback(response.data);
+				} else {
+					throw "Error response: " + JSON.stringify(response);
+				}
+			});
+		});
+	}
 })();
